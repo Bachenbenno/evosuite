20ed156e827f9f5ba6fc962d2f5adebcd5305a32
Sebastian Schweikl <schweikl@posteo.de>
2019-05-27 10:04:52 +0200

    Removed unneeded imports
    Signed-off-by: Sebastian Schweikl <schweikl@posteo.de>

--- client/src/main/java/org/evosuite/ga/Chromosome.java
+++ client/src/main/java/org/evosuite/ga/Chromosome.java
@@ -30,8 +30,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  * Abstract base class of chromosomes
