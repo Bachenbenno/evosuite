1ca80bd51c9f363fe11441d1f023ce22710ae594
Sebastian Schweikl <schweikl@posteo.de>
2019-06-17 19:44:36 +0200

    Refactoring: simplify implementation of "compare" method
    Signed-off-by: Sebastian Schweikl <schweikl@posteo.de>

--- client/src/main/java/org/evosuite/ga/comparators/OnlyCrowdingComparator.java
+++ client/src/main/java/org/evosuite/ga/comparators/OnlyCrowdingComparator.java
@@ -1,4 +1,4 @@
-/**
+/*
  *
  * This file is part of EvoSuite.
  *
@@ -25,16 +25,16 @@ import org.evosuite.ga.Chromosome;
 /**
  * This class implements a <code>Comparator</code> (a method for comparing <code>Chromosomes</code>
  * objects) based on the crowd distance of two chromosome objects.
- * 
+ *
  * @author Annibale Panichella, Fitsum M. Kifetew
  */
 public class OnlyCrowdingComparator implements Comparator<Chromosome>, Serializable {
-	
+
 	private static final long serialVersionUID = -6576898111709166470L;
 
 	/**
 	 * Compare the crowd distance value of two chromosome objects.
-	 * 
+	 *
 	 * @param c1 a {@link org.evosuite.ga.Chromosome} object
 	 * @param c2 a {@link org.evosuite.ga.Chromosome} object
 	 * @return -1 if crowd distance value of c1 is higher than the crowd distance of c2, 0 if the crowd
@@ -43,12 +43,6 @@ public class OnlyCrowdingComparator implements Comparator<Chromosome>, Serializa
 	 */
 	@Override
 	public int compare(Chromosome c1, Chromosome c2) {
-		if (c1.getDistance() > c2.getDistance()) {
-			return -1;
-		} else if (c1.getDistance() < c2.getDistance()) {
-			return +1;
-		} else { 
-			return 0;
-		}
+		return Double.compare(c2.getDistance(), c1.getDistance());
 	}
 }
