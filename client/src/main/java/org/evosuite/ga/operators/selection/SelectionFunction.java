2587d08043c426caf88005f7205113e04624886a
Sebastian Schweikl <schweikl@posteo.de>
2019-06-17 19:45:43 +0200

    Refactoring: implement probabilistic "select" via Stream.generate()
    Signed-off-by: Sebastian Schweikl <schweikl@posteo.de>

--- client/src/main/java/org/evosuite/ga/operators/selection/SelectionFunction.java
+++ client/src/main/java/org/evosuite/ga/operators/selection/SelectionFunction.java
@@ -22,13 +22,17 @@ package org.evosuite.ga.operators.selection;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.evosuite.ga.Chromosome;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Abstract base class of selection functions
+ * Abstract base class of selection functions. Can be used to choose the parents for mutation or
+ * crossover operations.
  *
  * @author Gordon Fraser
  */
@@ -74,11 +78,10 @@ public abstract class SelectionFunction<T extends Chromosome> implements Seriali
 	 * @return a {@link java.util.List} object.
 	 */
 	public List<T> select(List<T> population, int number) {
-		List<T> offspring = new ArrayList<>();
-		for (int i = 0; i < number; i++) {
-			offspring.add(population.get(getIndex(population)));
-		}
-		return offspring;
+		return Stream.generate(() -> getIndex(population))
+				.limit(number)
+				.map(population::get)
+				.collect(Collectors.toList());
 	}
 
 	/**
